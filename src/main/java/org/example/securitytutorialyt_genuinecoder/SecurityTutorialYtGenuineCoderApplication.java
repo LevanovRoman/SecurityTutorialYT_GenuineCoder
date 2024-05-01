d4f269eb604d50602fe7ea801e79a8deff43b082
levanovroman <levanovroman2016@yandex.ru>
2024-05-01 12:54:51 +0300

    complete

---
+++ src/main/java/org/example/securitytutorialyt_genuinecoder/SecurityTutorialYtGenuineCoderApplication.java
@@ -0,0 +1,13 @@
+package org.example.securitytutorialyt_genuinecoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityTutorialYtGenuineCoderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityTutorialYtGenuineCoderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/securitytutorialyt_genuinecoder/SecurityTutorialYtGenuineCoderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.securitytutorialyt_genuinecoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityTutorialYtGenuineCoderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
